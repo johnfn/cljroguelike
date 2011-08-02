d4676113b16a4d5805c7822ec26b493cadb3d1f6
Grant Mathews <grant@causes.com>
2011-08-01 23:07:52 -0700

    Hack hack hack. Apologies for the horrible commit messages.

--- src/cljroguelike/core.clj
+++ src/cljroguelike/core.clj
@@ -31,35 +31,31 @@
 ;          next-sq 
 ;    (let [start [(rand-int size) 0]]
 
+(defn merge-line [onto-line new-line ignore-char]
+  (map (fn [ontoch newch] (if (= newch ignore-char) ontoch newch)))
+  onto-line new-line)
+
+(defn merge-maps [onto new ignore-char]
+  "copies every char from NEW onto ONTO if that char != ignore-char"
+
+(defn make-map [size]
+  (->
+   (vec (for [x (range size)]
+          (apply str (repeat size "0"))))
+
+   ))
+
 (def map1
-  [
-    "01000000000000000000"
-    "01000000000000000000"
-    "00100000000000000000"
-    "00010000000000000000"
-    "00010000000000000000"
-    "00001000000000000000"
-    "00001000000000000000"
-    "00001100000000000000"
-    "00000100000001110000"
-    "00000010001110000000"
-    "00000001110000000000"
-    "00000000100000000000"
-    "00000000010000000000"
-    "00000000010000000000"
-    "00000000001000000000"
-    "00000000001000000000"
-    "00000000001100000000"
-    "00000000000010000000"
-    "00000000000000000000"
-    "00000000000000000000"
-    ])
+  (make-map 20))
 
 (defn draw-tile [x-rel y-rel type gfx]
   (let [y-offs 25 x-offs 10]
-    (.setColor gfx (if (= type \1) 
-                     (java.awt.Color/BLUE)
-                     (java.awt.Color/GREEN)))
+    (.setColor gfx (cond
+                    (= type \0) (java.awt.Color/GREEN)
+                    (= type \1) (java.awt.Color/BLUE)
+                    (= type \c) (java.awt.Color/WHITE)
+                    (= type \e) (java.awt.Color/BLACK)
+                    ))
     (.fillRect gfx (+ x-offs (* x-rel *t-width*))
                    (+ y-offs (* y-rel *t-width*))
                    *t-width* *t-width*)))
@@ -76,19 +72,8 @@
   (.fillRect gfx 0 0 *size* *size*)
   (render-map gfx)
 
- 
-  ;(println (first state))
-  (let [item (first state)]
-    (draw-tile (:x item) (:y item) \1 gfx))
-
   (doseq [item state]
-    (do 
-      (draw-tile (:x item) (:y item) \1 gfx)))
-  ;(for [item state]
-  ;  (println (:x item) (:y item) "1" gfx))
-  
-  (for [item state]
-    (draw-tile (:x item) (:y item) "1" gfx))
+    (draw-tile (:x item) (:y item) \c gfx))
   )
 
 (defn double-buffer-game [p state]
@@ -105,14 +90,14 @@
 (defmulti update (fn[x] (:type x)))
 
 (defn in-bounds [x y]
-  (and (< x (- (count map1) 1))
-       (< y (- (count map1) 1))
-       (> x 0)
-       (> x 0)))
+  (and (< x (count map1))
+       (< y (count map1))
+       (>= x 0)
+       (>= y 0)))
 
 (defn wall [x y]
-  (and (in-bounds x y)
-       (= \1 (nth (nth map1 y) x))))
+  (or (not (in-bounds x y))
+      (= \1 (nth (nth map1 y) x))))
 
 (defmethod update :player [player]
   (let [old-x (:x player)
@@ -130,15 +115,12 @@
   enemy)
 
 (defn core-loop [frame state]
-  (if (not (empty? @keys-down))
-    (println @keys-down))
-
   (double-buffer-game frame state)
   (Thread/sleep 5)
   (.setTitle frame "HURF."))
 
 (defn skeleton-loop [frame]
-  (loop [state [{:type :player :x 0 :y 10}
+  (loop [state [{:type :player :x 0  :y 10}
                 {:type :enemy  :x 0  :y 2}
                 ]]
     (core-loop frame state)
